7d21157eb0b11e453ba7443d204966b4be9fc58f
Dmitrii Tikhomirov <chani.liet@gmail.com>
2024-10-15 13:01:48 -0700

    rebase and post review update

--- internal/controller/validation/image_validator.go
+++ internal/controller/validation/image_validator.go
@@ -1,16 +1,21 @@
-// Copyright 2024 Apache Software Foundation (ASF)
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
 package validation
 
@@ -46,7 +51,7 @@ func (v *imageValidator) Validate(ctx context.Context, client client.Client, son
 		return err
 	}
 	if !equals {
-		return fmt.Errorf("Workflow, defined in the image %s doesn't match deployment workflow", sonataflow.Spec.PodTemplate.Container.Image)
+		return fmt.Errorf("Workflow, defined in the image %s doesn't match deployment workflow %s", sonataflow.Spec.PodTemplate.Container.Image, sonataflow.Name)
 	}
 	return nil
 }
@@ -72,7 +77,7 @@ func validateImage(ctx context.Context, sonataflow *operatorapi.SonataFlow) (boo
 		return false, err
 	}
 
-	reader, err := readWorkflowSpecLayer(ref)
+	reader, err := readWorkflowSpecLayer(ref, sonataflow)
 	if err != nil {
 		return false, err
 	}
@@ -118,7 +123,7 @@ func kindRegistryImage(sonataflow *operatorapi.SonataFlow) (v1.Image, error) {
 	return ref, nil
 }
 
-func readWorkflowSpecLayer(image v1.Image) (*tar.Reader, error) {
+func readWorkflowSpecLayer(image v1.Image, sonataflow *operatorapi.SonataFlow) (*tar.Reader, error) {
 	layers, err := image.Layers()
 	if err != nil {
 		return nil, err
@@ -131,7 +136,7 @@ func readWorkflowSpecLayer(image v1.Image) (*tar.Reader, error) {
 			return nil, err
 		}
 	}
-	return nil, fmt.Errorf("Workflow definition was not found in the Docker image")
+	return nil, fmt.Errorf("Workflow %s definition was not found in the Docker image", sonataflow.Name)
 }
 
 func findWorkflowSpecLayer(layer v1.Layer) (*tar.Reader, error) {
